8512add45d0dc3595788a21aa9f302f15a82a69a
JWMatheo <matmatvallone8@gmail.com>
2022-12-08 15:39:37 +0200

    add admin panel design

--- client/src/components/AdminPage/AdminPage.js
+++ client/src/components/AdminPage/AdminPage.js
@@ -42,32 +42,64 @@ function AdminPage({
               <div className="admin-content-1-container">
                 <div className="admin-content-1-wrap">
                   <span>Mint partenaires</span>
-                  <input type="text" />
-
-                  <button>Mint</button>
+                  <div>
+                    <select
+                      onChange={handleNumberChange}
+                      value={number}
+                      id="mint"
+                    >
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                    </select>
+                    <button>Mint</button>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="admin-content-2">
               <div className="admin-content-2-price">
-                <div>
-                  <span>Le prix du mint est de ? ETH </span>
-                  <button>Check price</button>
+                <div className="admin-content-2-price-container">
+                  <div className="admin-content-2-price-wrap">
+                    <span>Le prix du mint est de ? ETH </span>
+                    <button>Check price</button>
+                  </div>
                 </div>
-                <div>
-                  <span>Check this website to convert eth in wei</span>
-                  <input type="text" />
-                  <button>Set new price</button>
+                <div className="admin-content-2-price-container">
+                  <div className="admin-content-2-price-wrap">
+                    Change price
+                    <a href="https://eth-converter.com/" target="_blank">
+                    ( Convert eth in wei )
+                    </a>
+                    <div>
+                      <input type="text" placeholder="new price in wei" />
+                      <button>Set new price</button>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="admin-content-2-uri">
-                <div>
-                  <span>Current URI : ?</span>
-                  <button>Get URI</button>
+                <div className="admin-content-2-container">
+                  <div className="admin-content-2-wrap">
+                    <span>Current URI : ?</span>
+                    <button>Get URI</button>
+                  </div>
                 </div>
-                <div>
-                  <input type="text" placeholder="new uri" />
-                  <button>Set new URI</button>
+                <div className="admin-content-2-container">
+                  <div className="admin-content-2-wrap">
+                    <span>Change URI</span>
+                    <div>
+                      <input type="text" placeholder="new uri" />
+                      <button>Set new URI</button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
