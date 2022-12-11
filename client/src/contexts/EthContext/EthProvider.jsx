6ea50ae26588a3e145a6407aa354fd76a106331e
codegenius2 <lucifer231220@gmail.com>
2022-12-11 04:56:49 +0200

    affichage tel

--- client/src/contexts/EthContext/EthProvider.jsx
+++ client/src/contexts/EthContext/EthProvider.jsx
@@ -54,17 +54,17 @@ function EthProvider({ children }) {
     tryInit();
   }, [init]);
 
-  useEffect(() => {
-    const events = ["chainChanged", "accountsChanged"];
-    const handleChange = () => {
-      init(state.artifact);
-    };
+  // useEffect(() => {
+  //   const events = ["chainChanged", "accountsChanged"];
+  //   const handleChange = () => {
+  //     init(state.artifact);
+  //   };
 
-    events.forEach(e => window.ethereum.on(e, handleChange));
-    return () => {
-      events.forEach(e => window.ethereum.removeListener(e, handleChange));
-    };
-  }, [init, state.artifact]);
+  //   events.forEach(e => window.ethereum.on(e, handleChange));
+  //   return () => {
+  //     events.forEach(e => window.ethereum.removeListener(e, handleChange));
+  //   };
+  // }, [init, state.artifact]);
 
   return (
     <EthContext.Provider value={{
